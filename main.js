6d1a02dd73d994ddc34269d191ec26d0a251b024
Scott Doran <Scott.DORAN@health-on-line.co.uk>
2022-09-27 09:59:00 +0100

    Add project files.

---
+++ main.js
@@ -0,0 +1,71 @@
+import { Player } from "./player.js";
+import { InputHandler } from "./input.js";
+import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
+
+addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 700;
+  canvas.height = 500;
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.groundMargin = 85;
+      this.speed = 0;
+      this.maxSpeed = 3;
+      this.background = new Background(this);
+      this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
+      this.debug = true;
+    }
+    update(deltaTime) {
+      this.background.update();
+      this.player.update(this.input.keys, deltaTime);
+      // handleEnemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForDeletion)
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
+    }
+
+    draw(context) {
+      this.background.draw(context);
+      this.player.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+      this.enemies.push(new FlyingEnemy(this));
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+  let lastTime = 0;
+
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update(deltaTime);
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate(0);
+});
